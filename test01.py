0cd99de138c32fd9efedca0940cf682252229a0a
zhao <zhao_jian_hao@163.com>
2025-04-02 19:20:58 +0800

    创建项目
    新创建的项目

---
+++ test01.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+@File  : test01.py
+@Author: zhao
+@Date  : 2025/4/2 19:14
+@Desc  :  
+@Exp   : 
+'''
+
+#测试内容
+class A:
+    def __str__(self,name):
+        self.name=name
+
+class B(A):
+    pass
+
+
